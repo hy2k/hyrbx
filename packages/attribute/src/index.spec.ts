83e3ebc55341460bac3c77fb24cb6c59760e23f0
Yusuke Higuchi <hy2k@outlook.jp>
2023-12-25 11:15:18 +0900

    Fix package entry

--- packages/attribute/src/index.spec.ts
+++ packages/attribute/src/index.spec.ts
@@ -4,7 +4,7 @@
 type Expect<T extends true> = T;
 type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
 
-import { createAttribute } from './attribute';
+import { createAttribute } from './index';
 
 export = () => {
 	it('should get attribute correctly', () => {
