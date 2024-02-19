ef28f27d988ebb90f15972b4150d5d4eb759f28a
Yusuke Higuchi <hy2k@outlook.jp>
2024-02-19 16:31:56 +0900

    Upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,12 @@
-import { baseConfig, ignores, importConfig, perfConfig, robloxConfig, sortConfig, testConfig } from 'eslint-config-y2';
+import {
+	baseConfig,
+	ignoresConfig,
+	importConfig,
+	perfConfig,
+	robloxConfig,
+	sortConfig,
+	testConfig,
+} from 'eslint-config-y2';
 
 robloxConfig.languageOptions.parserOptions = {
 	...robloxConfig.languageOptions.parserOptions,
@@ -10,7 +18,7 @@ testConfig.files?.push('**/test.server.ts');
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
 	//
-	ignores,
+	ignoresConfig,
 	baseConfig,
 	robloxConfig,
 	importConfig,
